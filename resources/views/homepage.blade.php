ecb907d59d31be67d9f8c2e23b139593aeac4bef
JDare <jez433@gmail.com>
2019-01-13 17:11:30 -0800

    layout cleanup

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,65 +1,6 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-    <title>TITLE</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="UTF-8">
+@extends('layout')
 
-
-    <!-- Font -->
-
-    <link href="https://fonts.googleapis.com/css?family=Playball%7CBitter" rel="stylesheet">
-
-    <!-- Stylesheets -->
-
-    <link href="/css/bootstrap.css" rel="stylesheet">
-
-
-    <link href="/css/fluidbox.min.css" rel="stylesheet">
-
-    <link href="/css/font-icon.css" rel="stylesheet">
-
-
-    <link href="/css/styles.css" rel="stylesheet">
-
-    <link href="/css/responsive.css" rel="stylesheet">
-
-</head>
-<body>
-
-<header>
-
-    <div class="container">
-
-        <a class="logo" href="#"><img src="images/logo-white.png" alt="Logo"></a>
-
-        <div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="icon icon-bars"></i></div>
-
-        <ul class="main-menu visible-on-click" id="main-menu">
-            <li><a href="/">HOME</a></li>
-            {{--<li class="drop-down"><a href="#!">OUR STORIES<i class="icon icon-caret-down"></i></a>
-
-                <ul class="drop-down-menu">
-                    <li><a href="#">FEATURED</a></li>
-                    <li><a href="#">ABOUT</a></li>
-                    <li class="drop-down"><a href="#!">CATEGORIES<i class="icon icon-caret-right"></i></a>
-                        <ul class="drop-down-menu drop-down-inner">
-                            <li><a href="#">FEATURED</a></li>
-                            <li><a href="#">ABOUT</a></li>
-                            <li><a href="#">CATEGORIES</a></li>
-                        </ul>
-                    </li>
-                </ul>
-
-            </li>--}}
-            <li><a href="/about">THE WEDDING</a></li>
-        </ul><!-- main-menu -->
-
-    </div><!-- container -->
-</header>
-
-
+@section('content')
 <div class="main-slider">
     <div class="display-table center-text">
         <div class="display-table-cell">
@@ -118,7 +59,7 @@
         </div><!-- row -->
     </div><!-- container -->
 </section><!-- section -->
-
+@endsection
 {{-- <section class="section w-details-area center-text">
     <div class="container">
         <div class="row">
@@ -271,64 +212,3 @@
 
 </section><!-- section -->
 --}}
-
-<footer>
-    <div class="container center-text">
-
-        <div class="logo-wrapper">
-            <a class="logo" href="#"><img src="images/logo-black.png" alt="Logo Image"></a>
-            <i class="icon icon-star"></i>
-        </div>
-
-        {{--<div class="newsletter">
-            <h4 class="title">RECEIVE NEWS IN YOUR EMAIL</h4>
-            <p>Join our mailing list to receive news and announcements</p>
-            <div class="email-area">
-                <form>
-                    <input class="email-input" type="text" placeholder="Your Email">
-                    <button class="submit-btn" type="submit"><i class="icon icon-email-plane"></i></button>
-                </form>
-            </div><!-- email-area -->
-        </div><!-- newsletter -->
-        --}}
-
-        <ul class="social-icons">
-            <li><a href="#"><i class="icon icon-heart"></i></a></li>
-            <li><a href="#"><i class="icon icon-twitter"></i></a></li>
-            <li><a href="#"><i class="icon icon-instagram"></i></a></li>
-            <li><a href="#"><i class="icon icon-pinterest"></i></a></li>
-            <li><a href="#"><i class="icon icon-tripadvisor"></i></a></li>
-        </ul>
-
-        <ul class="footer-links">
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">OUR STORIES</a></li>
-            <li><a href="#">THE WEDDING</a></li>
-            <li><a href="#">GALLERY</a></li>
-            <li><a href="#">CONTACT</a></li>
-        </ul>
-
-        <p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-
-    </div><!-- container -->
-</footer>
-
-
-<!-- SCRIPTS -->
-
-<script src="/js/jquery-3.1.1.min.js"></script>
-
-<script src="/js/tether.min.js"></script>
-
-<script src="/js/bootstrap.js"></script>
-
-<script src="/js/jquery.countdown.min.js"></script>
-
-<script src="/js/jquery.fluidbox.min.js"></script>
-
-<script src="/js/scripts.js"></script>
-
-</body>
-</html>
