fb944d9c491d1c69eda4d21b55f3a50125b6ffd3
JDare <jez433@gmail.com>
2019-01-13 18:11:24 -0800

    update to prod docker config

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 git pull origin master
 docker-compose down
 docker-compose build
-docker-compose up -d
+docker-compose up -d -f docker-compose.yml -f docker-compose.prod.yml
 docker exec -it ww_app composer install -d /app
 docker exec -it ww_app php /app/artisan migrate --force
