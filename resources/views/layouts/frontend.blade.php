3ecabfc1b1209ef076fc8c06779c718bb33134a3
JDare <jez433@gmail.com>
2019-02-20 13:55:01 -0800

    added GTM tracking

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
+@if(env('GTM_ID'))
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{{ env('GTM_ID') }}');</script>
+<!-- End Google Tag Manager -->
+@endif
     <title>{{ config('wedding.bride.name') }} & {{ config('wedding.groom.name') }}'s Wedding {{ Carbon\Carbon::parse(config('wedding.date'))->format('Y') }} @yield('title')</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +30,12 @@
 
 </head>
 <body>
+@if (env('GTM_ID'))
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GTM_ID') }}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+@endif
 <header>
 
     <div class="container">
