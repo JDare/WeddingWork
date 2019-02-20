9037f86b1960f8a75cfbb0a72f5ec86e504dfdcc
JDare <jez433@gmail.com>
2019-02-19 20:23:10 -0800

    revalidating email

--- app/Http/Requests/StoreParty.php
+++ app/Http/Requests/StoreParty.php
@@ -26,6 +26,7 @@ class StoreParty extends FormRequest
         return [
             'name'      => 'required',
             'guests'    => 'required|array|min:1',
+            'email'     => 'nullable|email'
         ];
     }
 }
